afe5f83adea0fb9435c16288ea8365ba36dd82ab
Manish Singh Bisht <manisbst123@gmail.com>
2024-12-17 02:25:28 +0530

    inital commit

---
+++ public/js/stripe.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe(
+  'pk_test_51QV6bXAudVCX5mOfPj89rhlnViHZlbNkYX63vIVP7cfbmnngjQIQZxw6fQCueMFjR7ALQ5YsPQaIk5Q7BeRidQMh00kmarCpyG'
+);
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+
+    // 2) Create checkout form + charge credit card
+    const checkoutPageUrl = session.data.session.url;
+    window.location.assign(checkoutPageUrl);
+  } catch (error) {
+    showAlert('error', error);
+  }
+};
